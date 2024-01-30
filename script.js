dbd31da1cc09c469c24298828b221ca1af45575f
Noah-Pesendorfer <noah.pesendorfer@htl-wels.at>
2024-01-30 13:12:29 +0100

    last updated angepasst und benutzerfreundlich gemacht

--- script.js
+++ script.js
@@ -94,8 +94,8 @@ function updatePinnedItems() {
             document.getElementById('title').dataset.noteId = noteObj.id;
             document.getElementById('content').innerHTML = noteObj.body;
             document.getElementById('content').dataset.noteId = noteObj.id;
-            let options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', time: "numeric" };
-            let noteDate = noteObj.lastUpdated.toLocaleDateString("en-us", options)
+            let options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+            let noteDate = noteObj.lastUpdated.toLocaleDateString("en-us")
             document.getElementById('last-updated').innerHTML = noteDate;
 
             document.getElementById('content').focus()
