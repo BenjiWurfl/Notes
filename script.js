fb2bda60069f62d1982910e82cc8411f42a5630c
Noah-Pesendorfer <noah.pesendorfer@htl-wels.at>
2024-02-08 20:10:27 +0100

    Design von addNote button

--- script.js
+++ script.js
@@ -43,14 +43,14 @@ const notes = JSON.parse(localStorage.getItem('notes') || '[]');
 function showNotes() {
     const navContent = document.querySelector('.nav-content');
     navContent.innerHTML = '';
-    /*navContent.innerHTML = '<li>\n' +
+    navContent.innerHTML = '<li>\n' +
         '                <button type="button" onclick="my_modal_1.showModal()" class="flex w-full p-2 text-white transition duration-75 rounded-lg group bg-blue-700 hover:text-white" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">\n' +
         '                    <svg class="flex-shrink-0 w-5 h-5 transition duration-75 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">\n' +
         '                        <path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"></path>\n' +
         '                    </svg>\n' +
         '                    <span class="flex-1 ms-3 text-left whitespace-nowrap">Add a Project</span>\n' +
         '                </button>\n' +
-        '            </li>';*/
+        '            </li>';
     projectsArr.length = 0;
     const user = auth.currentUser;
     if (user) {
