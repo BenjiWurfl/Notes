ef6c5198121d670873a0f569c7854f35f2db32e0
Noah-Pesendorfer <noah.pesendorfer@htl-wels.at>
2024-02-09 12:40:17 +0100

    onAuthStateChanged position geändert

--- script.js
+++ script.js
@@ -370,7 +370,7 @@ function loadNotesOfProject(project) {
         '                <button onclick="backToProjects()" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100"><svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">\n' +
         '                    <path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z"></path>                    </svg>\n' +
         '\n' +
-        '                    <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">$(project.title)</span>\n' +
+        '                    <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">' + project.title + '</span>\n' +
         '                </button>\n' +
         '            </li>';
     notesArr.length = 0;
