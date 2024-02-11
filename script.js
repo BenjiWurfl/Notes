cbda05e3143925ec937f6db10e14af304bb12fd5
Noah-Pesendorfer <noah.pesendorfer@htl-wels.at>
2024-02-11 21:56:51 +0100

    project cards

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ function addProjectToNavbar(project) {
         '                </div>\n' +
         '                <div>\n' +
         '                    <div class="flex items-center text-white justify-center">\n' +
-        '                        <svg  xmlns="http://www.w3.org/2000/svg" class="text-white" width="20" height="20" viewBox="0 0 24 24" stroke-width="1" fill="black" stroke-linecap="round" stroke-linejoin="round">\n' +
+        '                        <svg  xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" stroke-width="1" fill="white" stroke-linecap="round" stroke-linejoin="round">\n' +
         '                            <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm3.293 14.707L11 12.414V6h2v5.586l3.707 3.707-1.414 1.414z"></path>\n' +
         '                        </svg>\n' +
         '                        <p class="text-sm  ml-1">' + project.dueDate.toLocaleDateString("en-us") + '</p>\n' +
