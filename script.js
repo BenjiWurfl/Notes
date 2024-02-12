1ce4ce001fbe9553073efe3c64e49f0e1bc095c0
Noah-Pesendorfer <noah.pesendorfer@htl-wels.at>
2024-02-12 17:35:14 +0100

    sidebar responsive ENTFERNT

--- script.js
+++ script.js
@@ -91,6 +91,25 @@ function addNoteToNavbar(note) {
         noteTitle = noteTitle.substring(0, 12) + '...';
     }
 
+    const containerForNoteCards = document.querySelector('.container-for-cards');
+    containerForNoteCards.innerHTML = "";
+    const roundedDiv = document.createElement('div');
+    roundedDiv.classList.add('rounded');
+    roundedDiv.innerHTML = '<div class="w-full h-32 flex flex-col justify-between bg-[#3019bd] rounded-lg border border-yellow-400 mb-6 py-5 px-4">\n' +
+        '                <div>\n' +
+        '                    <h4 class="text-white text-center font-bold mb-3">' + noteTitle + '</h4>\n' +
+        '                </div>\n' +
+        '                <div>\n' +
+        '                    <div class="flex items-center text-white justify-center">\n' +
+        '                        <svg  xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" stroke-width="1" fill="white" stroke-linecap="round" stroke-linejoin="round">\n' +
+        '                            <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm3.293 14.707L11 12.414V6h2v5.586l3.707 3.707-1.414 1.414z"></path>\n' +
+        '                        </svg>\n' +
+        '                        <p class="text-sm  ml-1">' + note.lastUpdated.toLocaleDateString("en-us") + '</p>\n' +
+        '                    </div>\n' +
+        '                </div>\n' +
+        '            </div>'
+    containerForNoteCards.appendChild(roundedDiv);
+
     pinnedNote.innerHTML = `<svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">
                                 <path d="M12.186 14.552c-.617 0-.977.587-.977 1.373 0 .791.371 1.35.983 1.35.617 0 .971-.588.971-1.374 0-.726-.348-1.349-.977-1.349z"></path><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zM9.155 17.454c-.426.354-1.073.521-1.864.521-.475 0-.81-.03-1.038-.06v-3.971a8.16 8.16 0 0 1 1.235-.083c.768 0 1.266.138 1.655.432.42.312.684.81.684 1.522 0 .775-.282 1.309-.672 1.639zm2.99.546c-1.2 0-1.901-.906-1.901-2.058 0-1.211.773-2.116 1.967-2.116 1.241 0 1.919.929 1.919 2.045-.001 1.325-.805 2.129-1.985 2.129zm4.655-.762c.275 0 .581-.061.762-.132l.138.713c-.168.084-.546.174-1.037.174-1.397 0-2.117-.869-2.117-2.021 0-1.379.983-2.146 2.207-2.146.474 0 .833.096.995.18l-.186.726a1.979 1.979 0 0 0-.768-.15c-.726 0-1.29.438-1.29 1.338 0 .809.48 1.318 1.296 1.318zM14 9h-1V4l5 5h-4z"></path><path d="M7.584 14.563c-.203 0-.335.018-.413.036v2.645c.078.018.204.018.317.018.828.006 1.367-.449 1.367-1.415.006-.84-.485-1.284-1.271-1.284z"></path>                    
                             </svg>
@@ -103,6 +122,9 @@ function addNoteToNavbar(note) {
 }
 
 function loadDataOfNote(note) {
+    const richTextEditor = document.querySelector('.textEditor');
+    richTextEditor.classList.remove('hidden');
+
     document.getElementById('title').innerHTML = note.title;
     document.getElementById('title').dataset.noteId = note.id;
     document.getElementById('text-content').innerHTML = note.body;
@@ -378,9 +400,6 @@ function flipDropdown(project) {
 
 
 function loadNotesOfProject(project) {
-
-    const richTextEditor = document.querySelector('.textEditor');
-    richTextEditor.classList.remove('hidden');
 
     const navContent = document.querySelector('.nav-content');
 
