cfb0662f100af6ec6b300fd9f23e92d65010554d
Noah-Pesendorfer <noah.pesendorfer@htl-wels.at>
2024-02-13 22:08:14 +0100

    hovercolor bg

--- script.js
+++ script.js
@@ -146,7 +146,7 @@ function updatePinnedNotes(project) {
     backToProjectsButton.addEventListener('click', () => backToProjects());
 
     const title = document.createElement('div');
-    title.classList.add('flex', 'items-center', 'text-4xl', 'text-[#3019bd]', 'p-4', 'font-bold', 'text-center');
+    title.classList.add('flex', 'items-center', 'text-4xl', 'text-[#3019bd]', 'col-span-4', 'p-4', 'w-full', 'font-bold', 'text-center');
     title.innerHTML = 'Notes of  <span class="bg-[#3019bd] text-white rounded-2xl p-2 ml-3"> ' + project.title + '</span>';
     const addNoteButton = document.createElement('div');
     addNoteButton.classList.add('ml-4', 'bg-[#3019bd]', 'w-10', 'h-10', 'font-bold', 'text-xl', 'text-white', 'shadow-md', 'rounded', 'cursor-pointer', 'flex', 'justify-center', 'items-center')
@@ -276,6 +276,12 @@ document.getElementById('title').addEventListener('input', function () {
     note.lastUpdated = new Date();
     notesArr.push(note);
     updateNoteToFirestore(note);
+
+    const project = projectsArr.find((project) => project.id === note.parentProject); // Hier wurde project.id === note.parentProject korrigiert
+
+    console.log("Project: ", project);
+
+    loadNotesOfProject(project);
 });
 
 document.getElementById('text-content').addEventListener('input', function () {
