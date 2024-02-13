0277cdb2929979c1a2bc4921ef1f616a30a4dad9
Noah-Pesendorfer <noah.pesendorfer@htl-wels.at>
2024-02-13 12:52:03 +0100

    farbe

--- script.js
+++ script.js
@@ -155,7 +155,7 @@ function addProjectToNavbar(project, containerForProjectCards) {
         '                    <h4 class="text-white text-center font-bold">' + project.title + '</h4>' +
         '                </div>' +
         '                <div>' +
-        '                    <div class="flex items-center text-[#3019bd] justify-center mt-2">' +
+        '                    <div class="flex items-center text-[#3019bd] justify-center mt-5">' +
         '                        <svg  xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" stroke-width="1" fill="#3019bd" stroke-linecap="round" stroke-linejoin="round">' +
         '                            <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm3.293 14.707L11 12.414V6h2v5.586l3.707 3.707-1.414 1.414z"></path>' +
         '                        </svg>' +
