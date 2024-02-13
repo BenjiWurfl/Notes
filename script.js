c1467b64d69afd1c3c23f139ebc915bdc1b6ed09
Noah-Pesendorfer <noah.pesendorfer@htl-wels.at>
2024-02-13 10:07:13 +0100

    width für addproject

--- script.js
+++ script.js
@@ -226,23 +226,7 @@ function updatePinnedItems() {
         '                                         <path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"></path>' +
         '                                   </svg>' +
         '                               </div>';
-
-
-    const addProjectDiv = document.createElement('div');
-    addProjectDiv.classList.add('rounded', 'col-span-4');
-    addProjectDiv.innerHTML = '<div onclick="my_modal_1.showModal()" class="w-full h-25 justify-center bg-[#170E48] rounded-lg border mb-6 py-5 px-4 cursor-pointer">\n' +
-        '                <div>\n' +
-        '                    <h4 class="text-white text-center font-bold mb-3"> Add Project </h4>\n' +
-        '                </div>\n' +
-        '                <div class="flex items-center text-black justify-center">\n' +
-        '                        <svg  xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" stroke-width="1" fill="black" stroke-linecap="round" stroke-linejoin="round">\n' +
-        '                            <path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"></path>\n' +
-        '                        </svg>\n' +
-        '                        <p class="text-sm  ml-1"></p>\n' +
-        '                    </div>\n' +
-        '</div>';
     containerForProjectCards.appendChild(title);
-    containerForProjectCards.appendChild(addProjectDiv);
 
 
     projectsArr.forEach((project, index) => {
