8be96e5968f0ab1a39b8604ae565197b3bcd7623
Noah-Pesendorfer <noah.pesendorfer@htl-wels.at>
2024-02-14 15:36:53 +0100

    navbar hinzugefügt script.js

--- script.js
+++ script.js
@@ -416,3 +416,28 @@ document.querySelector('.back-to-notes').addEventListener('click', () => {
 function backToProjects() {
     showNotes();
 }
+
+//Navbar
+
+const body = document.querySelector('body'),
+    sidebar = body.querySelector('nav'),
+    toggle = body.querySelector(".toggle"),
+    searchBtn = body.querySelector(".search-box"),
+    modeSwitch = body.querySelector(".toggle-switch"),
+    modeText = body.querySelector(".mode-text");
+
+
+toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+})
+
+modeSwitch.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+    if (body.classList.contains("dark")) {
+        modeText.innerText = "Light mode";
+    } else {
+        modeText.innerText = "Dark mode";
+
+    }
+});
