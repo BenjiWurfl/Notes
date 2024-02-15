fdf909e4f0eaf3ed41348e90797572987245b1a7
Noah-Pesendorfer <noah.pesendorfer@htl-wels.at>
2024-02-15 08:02:38 +0100

    log entfernen

--- script.js
+++ script.js
@@ -145,9 +145,103 @@ function updatePinnedNotes(project) {
     backToProjectsButton.innerHTML = '< back to projects'
     backToProjectsButton.addEventListener('click', () => backToProjects());
 
+    const ratingBar = document.createElement('span');
+    ratingBar.innerHTML = '<!-- TW Elements is free under AGPL, with commercial license required for specific uses. See more details: https://tw-elements.com/license/ and contact us for queries at tailwind@mdbootstrap.com --> \n' +
+        '<ul\n' +
+        '  id="events-example"\n' +
+        '  class="my-1 flex list-none gap-1 p-0"\n' +
+        '  data-te-rating-init>\n' +
+        '  <li>\n' +
+        '    <span\n' +
+        '      class="text-primary [&>svg]:h-5 [&>svg]:w-5"\n' +
+        '      data-te-rating-icon-ref>\n' +
+        '      <svg\n' +
+        '        xmlns="http://www.w3.org/2000/svg"\n' +
+        '        fill="none"\n' +
+        '        viewBox="0 0 24 24"\n' +
+        '        stroke-width="1.5"\n' +
+        '        stroke="currentColor">\n' +
+        '        <path\n' +
+        '          stroke-linecap="round"\n' +
+        '          stroke-linejoin="round"\n' +
+        '          d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />\n' +
+        '      </svg>\n' +
+        '    </span>\n' +
+        '  </li>\n' +
+        '  <li>\n' +
+        '    <span\n' +
+        '      class="text-primary [&>svg]:h-5 [&>svg]:w-5"\n' +
+        '      data-te-rating-icon-ref>\n' +
+        '      <svg\n' +
+        '        xmlns="http://www.w3.org/2000/svg"\n' +
+        '        fill="none"\n' +
+        '        viewBox="0 0 24 24"\n' +
+        '        stroke-width="1.5"\n' +
+        '        stroke="currentColor">\n' +
+        '        <path\n' +
+        '          stroke-linecap="round"\n' +
+        '          stroke-linejoin="round"\n' +
+        '          d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />\n' +
+        '      </svg>\n' +
+        '    </span>\n' +
+        '  </li>\n' +
+        '  <li>\n' +
+        '    <span\n' +
+        '      class="text-primary [&>svg]:h-5 [&>svg]:w-5"\n' +
+        '      data-te-rating-icon-ref>\n' +
+        '      <svg\n' +
+        '        xmlns="http://www.w3.org/2000/svg"\n' +
+        '        fill="none"\n' +
+        '        viewBox="0 0 24 24"\n' +
+        '        stroke-width="1.5"\n' +
+        '        stroke="currentColor">\n' +
+        '        <path\n' +
+        '          stroke-linecap="round"\n' +
+        '          stroke-linejoin="round"\n' +
+        '          d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />\n' +
+        '      </svg>\n' +
+        '    </span>\n' +
+        '  </li>\n' +
+        '  <li>\n' +
+        '    <span\n' +
+        '      class="text-primary [&>svg]:h-5 [&>svg]:w-5"\n' +
+        '      data-te-rating-icon-ref>\n' +
+        '      <svg\n' +
+        '        xmlns="http://www.w3.org/2000/svg"\n' +
+        '        fill="none"\n' +
+        '        viewBox="0 0 24 24"\n' +
+        '        stroke-width="1.5"\n' +
+        '        stroke="currentColor">\n' +
+        '        <path\n' +
+        '          stroke-linecap="round"\n' +
+        '          stroke-linejoin="round"\n' +
+        '          d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />\n' +
+        '      </svg>\n' +
+        '    </span>\n' +
+        '  </li>\n' +
+        '  <li>\n' +
+        '    <span\n' +
+        '      class="text-primary [&>svg]:h-5 [&>svg]:w-5"\n' +
+        '      data-te-rating-icon-ref>\n' +
+        '      <svg\n' +
+        '        xmlns="http://www.w3.org/2000/svg"\n' +
+        '        fill="none"\n' +
+        '        viewBox="0 0 24 24"\n' +
+        '        stroke-width="1.5"\n' +
+        '        stroke="currentColor">\n' +
+        '        <path\n' +
+        '          stroke-linecap="round"\n' +
+        '          stroke-linejoin="round"\n' +
+        '          d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />\n' +
+        '      </svg>\n' +
+        '    </span>\n' +
+        '  </li>\n' +
+        '</ul>'
+
+
     const editProjectButton = document.createElement('span');
     editProjectButton.classList.add('edit-project', 'px-4', 'w-screen', 'font-bold', 'text-[#3019bd]', 'text-md', 'cursor-pointer', 'text-right', 'justify-end');
-    editProjectButton.innerHTML = '<svg class="justify-end text-right" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#3019BD">' +
+    editProjectButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#3019BD">' +
         '<path d="m7 17.013 4.413-.015 9.632-9.54c.378-.378.586-.88.586-1.414s-.208-1.036-.586-1.414l-1.586-1.586c-.756-.756-2.075-.752-2.825-.003L7 12.583v4.43zM18.045 4.458l1.589 1.583-1.597 1.582-1.586-1.585 1.594-1.58zM9 13.417l6.03-5.973 1.586 1.586-6.029 5.971L9 15.006v-1.589z"></path><path d="M5 21h14c1.103 0 2-.897 2-2v-8.668l-2 2V19H8.158c-.026 0-.053.01-.079.01-.033 0-.066-.009-.1-.01H5V5h6.847l2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2z"></path>' +
         '</svg>';
     editProjectButton.addEventListener('click', () => editProject(project));
@@ -161,6 +255,7 @@ function updatePinnedNotes(project) {
     addNoteButton.addEventListener('click', () => addNewNote(project));
     title.append(addNoteButton);
     containerForNoteCards.appendChild(backToProjectsButton);
+    containerForNoteCards.appendChild(ratingBar);
     containerForNoteCards.appendChild(editProjectButton);
     containerForNoteCards.appendChild(title);
 
