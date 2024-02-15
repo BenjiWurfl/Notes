6c9d5923d45ed50f85af4cc606ec775f7e02ff38
Noah-Pesendorfer <noah.pesendorfer@htl-wels.at>
2024-02-15 08:30:42 +0100

    log entfernen

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ import {
     doc,
     Timestamp
 } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+import {getAuth, onAuthStateChanged} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 
 
 const firebaseConfig = {
