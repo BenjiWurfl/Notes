463112ad6c0e2d010a36909edd6dec143918df1d
Noah-Pesendorfer <noah.pesendorfer@htl-wels.at>
2024-02-18 11:40:09 +0100

    Align editPRoject right (ZUMINDEST PROBIER IS WEILS EH NED FUNKTIONIERT) UND LÖSCHEN JAAAAAA I WÜ NIMMA

--- script.js
+++ script.js
@@ -145,12 +145,23 @@ function updatePinnedNotes(project) {
     backToProjectsButton.addEventListener('click', () => backToProjects());
 
 
+    const modifyProjectSpan = document.createElement('span');
+    modifyProjectSpan.classList.add('modify-project', 'col-end-5', 'px-4', 'w-full', 'font-bold', 'text-[#3019bd]', 'text-md', 'cursor-pointer', 'text-right', 'justify-end', 'items-end');
+
     const editProjectButton = document.createElement('span');
-    editProjectButton.classList.add('edit-project', 'col-end-5', 'px-4', 'w-full', 'font-bold', 'text-[#3019bd]', 'text-md', 'cursor-pointer', 'text-right', 'justify-end', 'items-end');
     editProjectButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#3019BD">' +
         '<path d="m7 17.013 4.413-.015 9.632-9.54c.378-.378.586-.88.586-1.414s-.208-1.036-.586-1.414l-1.586-1.586c-.756-.756-2.075-.752-2.825-.003L7 12.583v4.43zM18.045 4.458l1.589 1.583-1.597 1.582-1.586-1.585 1.594-1.58zM9 13.417l6.03-5.973 1.586 1.586-6.029 5.971L9 15.006v-1.589z"></path><path d="M5 21h14c1.103 0 2-.897 2-2v-8.668l-2 2V19H8.158c-.026 0-.053.01-.079.01-.033 0-.066-.009-.1-.01H5V5h6.847l2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2z"></path>' +
         '</svg>';
     editProjectButton.addEventListener('click', () => editProject(project));
+
+    const deleteProjectButton = document.createElement('span');
+    deleteProjectButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="#3019BD">' +
+        '   <path d="M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z"></path>' +
+        '   <path d="M9 10h2v8H9zm4 0h2v8h-2z"></path>' +
+        '</svg>';
+    deleteProjectButton.addEventListener('click', () => deleteProject(project));
+
+
 
     const title = document.createElement('div');
     title.classList.add('flex', 'items-center', 'text-4xl', 'text-[#3019bd]', 'col-span-4', 'px-4', 'w-full', 'font-bold', 'text-center');
@@ -161,13 +172,24 @@ function updatePinnedNotes(project) {
     addNoteButton.addEventListener('click', () => addNewNote(project));
     title.append(addNoteButton);
     containerForNoteCards.appendChild(backToProjectsButton);
-    containerForNoteCards.appendChild(editProjectButton);
+
+    modifyProjectSpan.appendChild(editProjectButton);
+    containerForNoteCards.appendChild(modifyProjectSpan);
     containerForNoteCards.appendChild(title);
 
     console.log("Update")
     notesArr.forEach((note, index) => {
         addNoteToNavbar(note, containerForNoteCards);
     })
+}
+
+function deleteProject(project) {
+    let confirmDelete = confirm("Are you sure you want to delete this project?");
+    if (!confirmDelete) return;
+    const user = auth.currentUser;
+    const projRef = collection(db, "users", user.uid, "projects", project.id)
+    deleteDoc(projRef).then(() => showNotes())
+        .catch(error => console.error(error));
 }
 
 function updatePinnedItems() {
