3784f21d0bcea35cf9dd0364f8bfbe5c31f39fa1
Noah-Pesendorfer <noah.pesendorfer@htl-wels.at>
2024-02-19 11:16:12 +0100

    log

--- script.js
+++ script.js
@@ -227,12 +227,12 @@ function sortNotes(project, sortBy) {
         case "Date down":
             sortArrayByDateUp(project);
             sortBy.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill='#3019bd'><path d=\"M11 9h9v2h-9zm0 4h7v2h-7zm0-8h11v2H11zm0 12h5v2h-5zm-6 3h2V8h3L6 4 2 8h3z\"></path></svg>" +
-                "Date";
+                "Date up";
             break;
         case "Date up":
             sortArrayByDateDown(project);
             sortBy.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill='#3019bd'><path d=\"m6 20 4-4H7V4H5v12H2zm5-12h9v2h-9zm0 4h7v2h-7zm0-8h11v2H11zm0 12h5v2h-5z\"></path></svg>" +
-                "Date";
+                "Date down";
             break;
         default:
             sortArrayByDateDown(project);
