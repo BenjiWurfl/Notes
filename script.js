01077adfe877ecb5fc7dd62448c7794c7d5fa479
Noah-Pesendorfer <noah.pesendorfer@htl-wels.at>
2024-02-22 10:19:16 +0100

    delete project angepasst

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ import {
     getDocs,
     getFirestore,
     updateDoc,
-    recursiveDelete
 } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 import {getAuth, onAuthStateChanged} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 
